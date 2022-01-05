030f3d290683b58ae9bf292f1cb097d8d7c92c46
335497137 <335497137@gapps.yrdsb.ca>
2022-01-05 10:43:53 -0500

    test

--- MyWorld.java
+++ MyWorld.java
@@ -3,7 +3,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 /**
  * Write a description of class MyWorld here.
  * 
- * @author (your name) 
+ * @author Isaac, Kenneth 
  * @version (a version number or a date)
  */
 public class MyWorld extends World
