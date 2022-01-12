0f8644065b1e426a4a53c7f3f6ca28fecf85c9ed
335497137 <335497137@gapps.yrdsb.ca>
2022-01-12 10:24:14 -0500

    created grid

--- mapGrid.java
+++ mapGrid.java
@@ -1,12 +1,10 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
 /**
- * Write a description of class levelOne here.
+ * Write a description of class mapGrid here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class levelOne extends World
+public class mapGrid  
 {
     private static ImpassableBox impassableBox[] = new ImpassableBox[2304];
     int[] mapOne = {
@@ -35,30 +33,13 @@ public class levelOne extends World
                     1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
                     1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1
                     };
-    /*
     public void createMap(){
         for(int i = 0; i < 1152; i++){
             if(mapOne[i] == 1){
             impassableBox[i] = new ImpassableBox (24,24);
-            addObject(impassableBox[i], i%48 *24 +12 ,(int)Math.floor(i/48) * 24+12);
+            levelOne.addObject(impassableBox[i], i%48 *24 +12 ,(int)Math.floor(i/48) * 24+12);
             }
         }
-    }
-    */
-    /**
-     * Constructor for objects of class levelOne.
-     * 
-     */
-    public levelOne()
-    {    
-        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        //super((int)Math.round(512*2.5), (int)Math.round(288*2.5), 1);
-        super(384*3, 192*3, 1);
-        GreenfootImage bg = new GreenfootImage("untitled2.png");
-        bg.scale(384*3, 192*3);
-        setBackground(bg);
-        Player player = new Player();
-        addObject(player, 100, 100);
-        //createMap();
-    }
+    }                
+    
 }
